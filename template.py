ee38bffa2ad1b1a7a4d71b3d14e690019298445f
João Mano <joao.mano@outsystems.com>
2021-12-03 23:06:39 +0000

    Template

---
+++ template.py
@@ -0,0 +1,15 @@
+def parse_input(input):
+    return input
+
+def solve_part_1(input):
+    print("part1")
+
+def solve_part_2(input):
+    print("part2")
+
+if __name__ == '__main__':
+    with open('test_input.txt') as input:
+        lines = input.read().split('\n')
+
+    solve_part_1(lines)
+    solve_part_2(lines)
