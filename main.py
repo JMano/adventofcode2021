20c27d5b4a4cacfc05a0f2651293dfe8dcff8241
João Mano <joaomanooo@gmail.com>
2021-12-04 23:40:43 +0000

    Create files automatically

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import importlib
+import os
+from shutil import copyfile
 from get_input import get_input_for_day, get_day, get_test_input_for_day
 
 def load_input(args):
@@ -13,18 +15,28 @@ def load_input(args):
 
         return get_input_for_day(session, day)
 
+def create_files(day):
+    directory = 'Day' + day
+    file_path = directory + '/day' + day + '.py'
 
+    if not os.path.exists(file_path):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        copyfile('template.py', file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--day', help='Day of the challenge', default='1', required=False)
     parser.add_argument('-t', '--test', help='Use test input', default=False, action='store_true')
     args = parser.parse_args()
+    day_arg = args.day
+    day = get_day(day_arg)
+
+    create_files(day)
 
     input = load_input(args)
 
-    day_arg = args.day
-    day = get_day(day_arg)
 
     module = importlib.import_module('Day' + day + '.day' + day)
 
