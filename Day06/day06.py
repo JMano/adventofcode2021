ba005f68236c43de05f85596d9cc8e5e58a50823
João Mano <joao.mano@outsystems.com>
2021-12-09 22:56:24 +0000

    Day06 part 2

--- Day06/day06.py
+++ Day06/day06.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
+
 def parse_input(input):
     return [int(x) for x in input[0].split(",")]
 
-def nr_of_elements_after_x_days(initial_state: list[int], x):
+def nr_of_elements_after_x_days_part1(initial_state: list[int], x):
     elements = initial_state.copy()
     for _ in range(x):
         prev_list = elements.copy()
@@ -17,11 +19,34 @@ def nr_of_elements_after_x_days(initial_state: list[int], x):
 def solve_part_1(input):
     initial_state = parse_input(input)
 
-    fish_80 = nr_of_elements_after_x_days(initial_state, 80)
+    fish_80 = nr_of_elements_after_x_days_part1(initial_state, 80)
     print(len(fish_80)) # 388419
 
+def nr_of_elements_after_x_days_part2(initial_state: list[int], x):
+    elements = defaultdict(lambda: 0)
+    for state in initial_state:
+        elements[state] += 1
+
+    for _ in range(x):
+        new_elements = defaultdict(lambda: 0)
+        for key in range(9):
+            if key == 0:
+                new_elements[6] = elements[key]
+                new_elements[8] = elements[key]
+            elif key == 6 or key == 8:
+                new_elements[key - 1] = elements[key]
+            else:
+                new_elements[key - 1] += elements[key]
+
+        elements = new_elements
+
+    return elements
+
 def solve_part_2(input):
-    print("part2")
+    initial_state = parse_input(input)
+
+    fish_256 = nr_of_elements_after_x_days_part2(initial_state, 256)
+    print(sum(fish_256.values())) # 1740449478328 
 
 if __name__ == '__main__':
     with open('Day06/test_input.txt') as input:
